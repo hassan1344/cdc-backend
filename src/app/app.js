c98948a63866667de57fa93ffff5ead0df6a868e
Muhammad Hassan <60579417+hassan1344@users.noreply.github.com>
2025-06-09 18:57:47 +0200

    pipeline check

--- src/app/app.js
+++ src/app/app.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
 });
 
 app.get("/api/", (req, res) => {
-  return res.send("Service is available.");
+  return res.send("Service is available...");
 });
 
 //------------------Routes here------------------------------
