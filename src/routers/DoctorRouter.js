fb50b70098dc0b32f502a02a441fead1c474a96e
HUNAIN ARIF <hunainarif30@gmail.com>
2025-07-15 19:43:31 +0200

    added doctor router

--- src/routers/DoctorRouter.js
+++ src/routers/DoctorRouter.js
@@ -3,11 +3,10 @@ import DoctorController from "../controllers/DoctorController.js";
 
 export const DoctorRouter = Router();
 
+// GET - Get all doctors
+DoctorRouter.get("/", DoctorController.getAllDoctors);
 // PUT - Update doctor by doctor ID
 DoctorRouter.put("/:doctorId", DoctorController.updateDoctor);
 
 // GET - Get doctor by doctor ID
 DoctorRouter.get("/:doctorId", DoctorController.getDoctorById);
-
-// GET - Get all doctors
-DoctorRouter.get("/", DoctorController.getAllDoctors);
